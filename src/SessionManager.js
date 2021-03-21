d423e6c9925a8d16f0ab5651d6a625537ef5d042
Chris <chris@verida.io>
2021-03-21 17:05:52 +1030

    Support expiring requests with garbage collection

--- src/SessionManager.js
+++ src/SessionManager.js
@@ -6,12 +6,20 @@ dotenv.config()
 const APP_NAME = 'Vault: Auth Server'
 
 const connections = {}
+const requests = {}
+
 const veridaApp = new Verida({
     chain: process.env.CHAIN,
     address: process.env.ADDRESS,
     privateKey: process.env.PRIVATE_KEY,
     appName: APP_NAME
 })
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 class SessionManager {
 
@@ -21,6 +29,7 @@ class SessionManager {
 
         connections[sessionId] = socket
         console.log("Total sessions: " + Object.keys(connections).length)
+        console.log("Total pending requests: " + Object.keys(requests).length)
 
         return sessionId
     }
@@ -41,6 +50,7 @@ class SessionManager {
                 break
             case 'responseJwt':
                 const response = await this.processResponseJwt(message.sessionId, message.data)
+                delete requests[message.sessionId]
                 socket.send(JSON.stringify({
                     type: "auth-vault-response",
                     ...response
@@ -53,11 +63,24 @@ class SessionManager {
 
     async processResponseJwt(sessionId, encryptedClientResponse) {
         const clientSocket = connections[sessionId]
+        this.gc()
 
         if (!clientSocket) {
             return {
                 success: false,
-                message: `Unable to locate session: ${sessionId}`
+                code: 50,
+                message: `Unable to locate session, try reloading the QR code`
+            }
+        }
+
+        const request = requests[sessionId]
+
+        // Verify the request hasn't expired for this session
+        if (!request || this.hasRequestExpired(request)) {
+            return {
+                success: false,
+                coide: 51,
+                message: `Request has expired, try reloading the QR code`
             }
         }
 
@@ -74,7 +97,7 @@ class SessionManager {
     }
 
     async generateRequestJwt(veridaApp, sessionId, appName) {
-        const EXPIRY_OFFSET = process.env.EXPIRY_OFFSET
+        const EXPIRY_OFFSET = parseInt(process.env.EXPIRY_OFFSET)
         const AUTH_URI = process.env.AUTH_URI
         const LOGIN_DOMAIN = process.env.LOGIN_DOMAIN
         const now = Math.floor(Date.now() / 1000)
@@ -93,12 +116,41 @@ class SessionManager {
             appName: appName
         })
 
+        data.expiry = expiry
+        requests[sessionId] = data
+
         return didJwt
     }
 
     async close(sessionId) {
         console.log("Closing session: " + sessionId)
         delete connections[sessionId]
+    }
+
+    hasRequestExpired(request) {
+        // Verify the request hasn't expired for this session
+        const now = Math.floor(Date.now() / 1000)
+        const expiry = request.expiry
+
+        if (expiry < now) {
+            return true
+        }
+
+        return false
+    }
+
+    // Garbage collect expired requests 5% of the time
+    gc() {
+        const random = getRandomInt(0,20)
+        console.log('random', random)
+        if (random == 1) {
+            for (var i in requests) {
+                let request = requests[i]
+                if (this.hasRequestExpired(request)) {
+                    delete requests[i]
+                }
+            }
+        }
     }
 
 }
