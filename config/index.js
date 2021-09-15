b4b655fee61133e144679b738645d26d31724e15
Chris <chris@verida.io>
2021-09-15 10:57:03 +0930

    Change default private keys to avoid clash with old auth server.

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
         'Verida: Auth client demo': {
             chain: 'ethr',
             address: '0x92d7460891da517442d610faE80E717349072Fcd',
-            privateKey: '0x27d996ddda2f5131fb7567815c3df91c663b02d276d18c111017817319c370c8'
+            privateKey: '0x27d996ddda2f5131fb7567815c3df91c663b02d276d18c111017817319c370c9'
             //loginOrigin: 'https://localhost:8080'
         }
     },
     defaultContext: {
         chain: 'ethr',
         address: '0x92d7460891da517442d610faE80E717349072Fcd',
-        privateKey: '0x27d996ddda2f5131fb7567815c3df91c663b02d276d18c111017817319c370c8',
+        privateKey: '0x27d996ddda2f5131fb7567815c3df91c663b02d276d18c111017817319c370c9',
     }
 }
